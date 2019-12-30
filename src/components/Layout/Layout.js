68f9d87af8fc393e07d3523c0c84c03dbc4e8c80
MATRIX\Kamil <tomala.kamil@gmail.com>
2019-12-30 22:21:47 +0100

    Close SideDrawer when click to Backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,11 +6,21 @@ import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
   render() {
     return (
       <Aux>
         <Toolbar />
-        <SideDrawer />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler}
+        />
         <main className={classes.Content}>{this.props.children}</main>
       </Aux>
     );
