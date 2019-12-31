6acaed0e74f4923bdd24d1ab2eaaced27f61aef2
Kamil <tomala.kamil@gmail.com>
2019-12-31 13:39:38 +0100

    Add Menu component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,13 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import classes from "./Toolbar.module.css";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import Menu from "../../Menu/Menu";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>Menu</div>
+    <div>
+      <Menu />
+    </div>
     <div className={classes.Logo}>
       <Logo />
     </div>
