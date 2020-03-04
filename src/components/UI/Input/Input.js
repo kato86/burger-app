cc33f6752d06b9016778cf56bc520e007d51d858
Kamil <tomala.kamil@gmail.com>
2020-03-04 15:44:47 +0100

    Error messages

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,10 +4,14 @@ import classes from "./Input.module.css";
 
 const input = props => {
   let inputElement = null;
+  let validationError = null;
   const inputClasses = [classes.InputElement];
 
   if (props.invalid && props.shouldValidate && props.touched) {
     inputClasses.push(classes.Invalid);
+    validationError = (
+      <p className={classes.Error}>Please enter a valid value!</p>
+    );
   }
 
   switch (props.elementType) {
@@ -62,6 +66,7 @@ const input = props => {
     <div className={classes.Input}>
       <label className={classes.Label}>{props.label}</label>
       {inputElement}
+      {validationError}
     </div>
   );
 };
