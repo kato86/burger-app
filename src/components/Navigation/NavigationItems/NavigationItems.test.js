14cc183a53d7ccbcb84b24bfd227557799a99915
Kamil <tomala.kamil@gmail.com>
2020-07-24 14:24:48 +0200

    Testing components

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,8 +8,18 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 configure({ adapter: new Adapter() });
 
 describe("<NavigationItems />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
   it("should render two <NavigationItems /> elements if not authenticated", () => {
-    let wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it("should render three <NavigationItems /> elements if authenticated", () => {
+    wrapper = shallow(<NavigationItems isAuthenticated />);
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
